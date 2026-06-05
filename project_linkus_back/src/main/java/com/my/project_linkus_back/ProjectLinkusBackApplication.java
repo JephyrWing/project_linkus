4a377f672edcdf25d02b37ad24882fb49a00c915
JephyrWing <gad03302@gmail.com>
2026-06-05 17:18:58 +0900

    first setting 완료

---
+++ project_linkus_back/src/main/java/com/my/project_linkus_back/ProjectLinkusBackApplication.java
@@ -0,0 +1,13 @@
+package com.my.project_linkus_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLinkusBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLinkusBackApplication.class, args);
+	}
+
+}
---
+++ project_linkus_back/src/test/java/com/my/project_linkus_back/ProjectLinkusBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.project_linkus_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLinkusBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
