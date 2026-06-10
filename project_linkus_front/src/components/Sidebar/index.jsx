42335e404b5704d4d8d0f5588c15d82d3d4038ff
bgy030448 <bgy030448@gmail.com>
2026-06-10 16:56:50 +0900

    feat: sidebar 완성

--- project_linkus_front/src/components/Sidebar/index.jsx
+++ project_linkus_front/src/components/Sidebar/index.jsx
@@ -18,7 +18,7 @@ function Sidebar({ isOpen, onClose }) {
         <nav className="sidebar-menu">
           <a href="/">Home</a>
           <a href="/login">Login</a>
-          <a href="/signup">Sign</a>
+          <a href="/signup">Sign Up</a>
           <a href="/mypage">My Page</a>
         </nav>
       </aside>
