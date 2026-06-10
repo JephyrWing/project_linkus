8f075eff6bda8f7ce52ba9d66387f057d55c2a9a
JephyrWing <gad03302@gmail.com>
2026-06-10 11:13:21 +0900

    feat : entity 작성 완료

---
+++ project_linkus_back/src/main/java/com/my/project_linkus_back/chats/entity/Chats.java
@@ -0,0 +1,30 @@
+package com.my.project_linkus_back.chats.entity;
+
+import com.my.project_linkus_back.common.entity.BaseEntity;
+import com.my.project_linkus_back.users.entity.Users;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.locationtech.jts.geom.Point;
+
+
+@Entity
+@Getter
+@Setter
+public class Chats extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500)
+    private String text;
+
+    @Column(columnDefinition = "POINT")
+    private Point location;
+
+    private String ip;
+
+    @ManyToOne(fetch = FetchType.LAZY) // 필요할 때(호출할 때)만 로딩, on delete나 on update 설정은 괄호 안에 cascade= 으로 설정
+    @JoinColumn(name = "user_id")
+    private Users user;
+}
