cf713663b3debe30fa978c4b2454e5ad3de1c1cb
ckdwlsv <ckdwlsv@naver.com>
2026-06-10 16:04:17 +0900

    feat : login, signup 백엔드 작업 완료.

--- project_linkus_back/src/main/java/com/my/project_linkus_back/users/repository/UsersRepository.java
+++ project_linkus_back/src/main/java/com/my/project_linkus_back/users/repository/UsersRepository.java
@@ -13,8 +13,5 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
 
     //  회원 가입 시 아이디 중복 체크용
     boolean existsByUserId(String  userId);
-
-    // 회원 가입 시 닉네임 중복 체크용
-    boolean existsByNickName(String nickName);
-
+    
 }
--- project_linkus_back/src/main/java/com/my/project_linkus_back/users/service/UsersService.java
+++ project_linkus_back/src/main/java/com/my/project_linkus_back/users/service/UsersService.java
@@ -1,5 +1,7 @@
 package com.my.project_linkus_back.users.service;
 
+import com.my.project_linkus_back.users.dto.LoginDto;
+import com.my.project_linkus_back.users.dto.UserResponseDto;
 import com.my.project_linkus_back.users.dto.UsersSignupRequestDto;
 import com.my.project_linkus_back.users.entity.Users;
 import com.my.project_linkus_back.users.repository.UsersRepository;
@@ -48,5 +50,37 @@ public class UsersService {
         usersRepository.save(user);
 
         return dto;
+    }
+    // 로그인
+    public UserResponseDto login(LoginDto dto) {
+
+        Users user = usersRepository.findByUserId(dto.getUserId())
+                .orElseThrow(() ->
+                        new RuntimeException("존재하지 않는 아이디 입니다."));
+
+        boolean matches = passwordEncoder.matches(
+                dto.getPassword(),
+                user.getPassword()
+        );
+
+        if(!matches) {
+            throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+        }
+        return UserResponseDto.builder()
+                .Id(user.getId())
+                .userId(user.getUserId())
+                .nickName(user.getNickName())
+                .level(user.getLevel())
+                .build();
+    }
+    //회원탈퇴
+    public void deleteUser(Long id) {
+
+        //회원 존재 여부 확인
+        Users user = usersRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException(" 회원을 찾을 수 없습니다."));
+        //회원삭제
+        usersRepository.delete(user);
     }
 }
