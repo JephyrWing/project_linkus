3c4356850b3bf07171e556330752ce0db50c88e1
bgy030448 <bgy030448@gmail.com>
2026-06-10 14:43:22 +0900

    feat: Footer 기본 구조

--- project_linkus_front/src/App.jsx
+++ project_linkus_front/src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import Login from "./components/Login";
 import Signup from "./components/SignUp";
 import Navbar from "./components/Navbar";
-import Main from './components/Main';
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
+      <footer style={{ marginTop: "auto" }}>
+        <Footer />
+      </footer>
     </>
   );
 }
