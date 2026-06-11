f872170cb177b679868dd57338d139379cc79284
ckdwlsv <ckdwlsv@naver.com>
2026-06-11 12:18:21 +0900

    feat : Filters 작업

---
+++ project_linkus_back/src/main/java/com/my/project_linkus_back/filters/controller/FilterController.java
@@ -0,0 +1,32 @@
+package com.my.project_linkus_back.filters.controller;
+
+import com.my.project_linkus_back.filters.dto.FilterRequestDto;
+import com.my.project_linkus_back.filters.entity.Filters;
+import com.my.project_linkus_back.filters.service.FilterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/filters")
+@RequiredArgsConstructor
+public class FilterController {
+
+    private final FilterService filterService;
+
+    @PostMapping
+    public String addWord(@RequestBody FilterRequestDto dto) {
+        filterService.addWord(dto.getWord());
+        return "등록 완료";
+    }
+    @GetMapping
+    public List<Filters> getAllWords() {
+        return filterService.getAllWords();
+    }
+    @DeleteMapping("/{id}")
+    public String deleteWord(@PathVariable Long id){
+        filterService.deleteWord(id);
+        return "삭제 완료";
+    }
+}
