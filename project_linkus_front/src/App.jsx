b5e1dad49662db67f9a753a37403594543d1f69c
bgy030448 <bgy030448@gmail.com>
2026-06-12 09:58:23 +0900

    feat: 신고페이지 구축 및 사이드바에 신고버튼 추가

--- project_linkus_front/src/App.jsx
+++ project_linkus_front/src/App.jsx
@@ -9,8 +9,9 @@ import Update from "./components/MyPage/Update";
 import Records from "./components/MyPage/Records";
 import Sidebar from "./components/Sidebar";
 import AdminPage from "./components/AdminPage";
+import Report from "./components/Report";
 
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
@@ -20,36 +21,35 @@ function App() {
   /* isSidebarOpen =  사이드바가 열려 있는지 아닌지 저장하는 값*/
   /* setIsSidebarOpen = 그 값을 바꾸는 함수 */
   /* useState(false) =  처음에는 사이드바 닫힘*/
-  const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   // 로그인 상태
-  const [ user, setUser ] = useState({ isLogIn: true, role: 'admin'})  // 원래 값: true, admin
+  const [user, setUser] = useState({ isLogIn: true, role: "admin" }); // 원래 값: true, admin
 
   return (
     <>
-    {/* 사이드바 만들기 */}
-    {/* ()=>setIsSidebarOpen(true) : 실행되면 isSidebarOpen 값을 true로 바꾼다 */}
-    {/* isSidebarOpen = true : 사이드바 열림 */}
+      {/* 사이드바 만들기 */}
+      {/* ()=>setIsSidebarOpen(true) : 실행되면 isSidebarOpen 값을 true로 바꾼다 */}
+      {/* isSidebarOpen = true : 사이드바 열림 */}
       <Navbar onMenuClick={() => setIsSidebarOpen(true)} />
-        <Sidebar
+      <Sidebar
         isOpen={isSidebarOpen}
-        onClose={() => setIsSidebarOpen(false)} 
+        onClose={() => setIsSidebarOpen(false)}
         user={user}
         setUser={setUser}
-        />
+      />
 
       <main>
-
-      <Routes>
-        /* 네브바에 링크 연결하기 위해 라우트 사용 */
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/mypage/update" element={<Update />} />
-        <Route path="/mypage/records" element={<Records />} />
-        <Route path="/adminpage" element={<AdminPage />} />
-      </Routes>
-      
+        <Routes>
+          {/* 네브바에 링크 연결하기 위해 라우트 사용  */}
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/update" element={<Update />} />
+          <Route path="/mypage/records" element={<Records />} />
+          <Route path="/adminpage" element={<AdminPage />} />
+          <Route path="/report" element={<Report />} />
+        </Routes>
       </main>
 
       <footer style={{ marginTop: "auto" }}>
