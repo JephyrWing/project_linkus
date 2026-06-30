b6053d46bd0bb953218689127311338b998ed048
JephyrWing <gad03302@gmail.com>
2026-06-30 19:50:30 +0900

    배포 수정 6차

--- project_linkus_front/src/store/useChatStore.js
+++ project_linkus_front/src/store/useChatStore.js
@@ -5,26 +5,7 @@ import { immer } from "zustand/middleware/immer";
 const useChatStore = create(
   immer((set) => ({
     // 채팅 리스트에 표시할 채팅(기존 채팅 + axios로 받아오는 채팅)
-    chatList: [
-      {
-        chatId: 21,
-        userId: 40,
-        text: "안녕하세요",
-        ip: null,
-        longitude: 127.028007118842,
-        latitude: 37.2772455336538,
-        createdAt: "2026-06-23 12:44:35",
-      },
-      {
-        chatId: 22,
-        userId: 41,
-        text: "여기 누구 있나요?",
-        ip: null,
-        longitude: 127.028007118842,
-        latitude: 37.2772455336538,
-        createdAt: "2026-06-23 12:45:35",
-      },
-    ],
+    chatList: [],
 
     // 맵에 표시할 채팅(axios로 받아오는 채팅리스트만 저장)
     mapChat: [],
