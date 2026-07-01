9c6a8ec8636db5fc2e04c6e64dc6831e3e5ea4e6
bgy030448 <bgy030448@gmail.com>
2026-07-01 11:37:10 +0900

    feat: 회원 정보수정이 완료될 시 홈화면으로 이동하게 수정

--- project_linkus_front/src/components/MyPage/Update.jsx
+++ project_linkus_front/src/components/MyPage/Update.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import getCommonApi from "../../utils/Axios/getCommonApi";
 import "./update.css";
 
-
 function Update() {
+  const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState({
     userId: "",
     currentPassword: "",
@@ -15,8 +15,8 @@ function Update() {
     gender: "Female",
     callNum: "",
     level: "",
-    chatCustom: ""
-  })
+    chatCustom: "",
+  });
 
   const userId = localStorage.getItem("userId"); // 저장된 아이디 가져오기
 
@@ -25,10 +25,10 @@ function Update() {
 
     const fetchUserData = async () => {
       try {
-        const response =await getCommonApi().get(`/users/my/${userId}`);
+        const response = await getCommonApi().get(`/users/my/${userId}`);
         setUserInfo(response.data);
       } catch (error) {
-        console.error("정보 조회 실패: ", error)
+        console.error("정보 조회 실패: ", error);
       }
     };
     fetchUserData();
@@ -36,7 +36,7 @@ function Update() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUserInfo({...userInfo, [name]: value});
+    setUserInfo({ ...userInfo, [name]: value });
   };
 
   const handleSubmit = async (e) => {
@@ -51,12 +51,15 @@ function Update() {
       kakaoAccountLink: userInfo.kakaoAccountLink || "",
       googleAccountLink: userInfo.googleAccountLink || "",
       currentPassword: userInfo.currentPassword,
-      newPassword: userInfo.newPassword
+      newPassword: userInfo.newPassword,
     };
 
     try {
-      await getCommonApi().put(`/users/my/${userId}`, updateData); // 서버로 수정된 정보 전송
+      await getCommonApi().put(`/users/my/${userId}`, updateData);
+
+      // alert 확인 버튼을 누른 다음 홈 화면으로 이동함
       alert("수정이 완료되었습니다.");
+      navigate("/");
     } catch (error) {
       console.error("수정 실패: ", error);
       alert(error.response?.data?.message || "수정에 실패했습니다.");
@@ -64,8 +67,8 @@ function Update() {
   };
 
   // 탈퇴하기
-  const handleDelete = async() => {
-    if(!window.confirm("정말 탈퇴하시겠습니까?")) {
+  const handleDelete = async () => {
+    if (!window.confirm("정말 탈퇴하시겠습니까?")) {
       return;
     }
 
@@ -77,60 +80,117 @@ function Update() {
       localStorage.removeItem("accessToken");
 
       alert("탈퇴가 완료되었습니다.");
-      window.location.href="/";  // 메인페이지로 이동
-    } catch(error) {
-      console.error("탈퇴 실패: ", error)
+      window.location.href = "/"; // 메인페이지로 이동
+    } catch (error) {
+      console.error("탈퇴 실패: ", error);
       alert("탈퇴 실패했습니다.");
     }
-  }
+  };
 
-
- return (
+  return (
     <div className="update-container">
       <h1 className="update-title">회원 정보 조회/수정</h1>
-      
+
       <form onSubmit={handleSubmit}>
         {/* 읽기 전용 영역 */}
         <div className="info-readonly">
-          <p><span>아이디:    </span> {userInfo.userId}</p>
-          <p><span>이메일:    </span> {userInfo.email}</p>
-          <p><span>레벨:    </span> {userInfo.level}</p>
+          <p>
+            <span>아이디: </span> {userInfo.userId}
+          </p>
+          <p>
+            <span>이메일: </span> {userInfo.email}
+          </p>
+          <p>
+            <span>레벨: </span> {userInfo.level}
+          </p>
         </div>
 
         {/* 수정 가능 영역 */}
         <div className="update-input-group">
           <label>현재 비밀번호</label>
-          <input type="password" name="currentPassword" className="input-field" value={userInfo.currentPassword} onChange={handleChange} />
-        </div>  
+          <input
+            type="password"
+            name="currentPassword"
+            className="input-field"
+            value={userInfo.currentPassword}
+            onChange={handleChange}
+          />
+        </div>
         <div className="update-input-group">
           <label>새 비밀번호</label>
-          <input type="password" name="newPassword" className="input-field" value={userInfo.newPassword} onChange={handleChange} />
-        </div>  
+          <input
+            type="password"
+            name="newPassword"
+            className="input-field"
+            value={userInfo.newPassword}
+            onChange={handleChange}
+          />
+        </div>
         <div className="update-input-group">
           <label>생년월일</label>
-          <input type="date" name="dateOfBirth" className="input-field" value={userInfo.dateOfBirth} onChange={handleChange} />
-        </div>  
-          <label className="update-label">성별</label>
-          <div className="radio-group">
-            <label><input type="radio" name="gender" value="Male" checked={userInfo.gender === "Male"} onChange={handleChange} /> 남자</label>
-            <label><input type="radio" name="gender" value="Female" checked={userInfo.gender === "Female"} onChange={handleChange} /> 여자</label>
-          </div>
+          <input
+            type="date"
+            name="dateOfBirth"
+            className="input-field"
+            value={userInfo.dateOfBirth}
+            onChange={handleChange}
+          />
+        </div>
+        <label className="update-label">성별</label>
+        <div className="radio-group">
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="Male"
+              checked={userInfo.gender === "Male"}
+              onChange={handleChange}
+            />{" "}
+            남자
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="Female"
+              checked={userInfo.gender === "Female"}
+              onChange={handleChange}
+            />{" "}
+            여자
+          </label>
+        </div>
         <div className="update-input-group">
           <label>전화번호</label>
-          
-          <input type="tel" name="callNum" className="input-field" value={userInfo.callNum} onChange={handleChange} placeholder="010-XXXX-XXXX" />
-        </div>  
+
+          <input
+            type="tel"
+            name="callNum"
+            className="input-field"
+            value={userInfo.callNum}
+            onChange={handleChange}
+            placeholder="010-XXXX-XXXX"
+          />
+        </div>
         <div className="update-input-group">
           <label>말풍선 커스텀</label>
-          <input type="text" name="chatCustom" className="input-field" value={userInfo.chatCustom} onChange={handleChange} />
+          <input
+            type="text"
+            name="chatCustom"
+            className="input-field"
+            value={userInfo.chatCustom}
+            onChange={handleChange}
+          />
         </div>
 
-        <button type="submit" className="submit-btn">수정 완료</button>
+        <button type="submit" className="submit-btn">
+          수정 완료
+        </button>
 
-        <button type="button" className="delete-btn" onClick={handleDelete}>탈퇴하기</button>
+        <button type="button" className="delete-btn" onClick={handleDelete}>
+          탈퇴하기
+        </button>
       </form>
     </div>
   );
-
 }
 export default Update;
