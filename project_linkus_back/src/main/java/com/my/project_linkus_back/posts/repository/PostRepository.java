a2631667e7dd9983ca7ec23c34dd268d1c24d01d
JephyrWing <gad03302@gmail.com>
2026-07-01 09:39:37 +0900

    fix : 게시물 좌표계 수정 완료

--- project_linkus_back/src/main/java/com/my/project_linkus_back/posts/repository/PostRepository.java
+++ project_linkus_back/src/main/java/com/my/project_linkus_back/posts/repository/PostRepository.java
@@ -11,13 +11,9 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Posts, Long> {
-    @Query(value = "SELECT * FROM Posts WHERE MBRContains(LineString(Point(:swLongitude, :swLatitude), Point(:neLongitude, :neLatitude)), POINT(ST_X(location), ST_Y(location)))", nativeQuery = true)
-    List<Posts> postsContainedCurrentMap(
-            @Param("swLatitude") String swLatitude,
-            @Param("swLongitude") String swLongitude,
-            @Param("neLatitude") String neLatitude,
-            @Param("neLongitude") String neLongitude
-    );
+    @Query(value = "SELECT * FROM Posts WHERE ST_Contains(ST_MakeEnvelope(POINT(:swLongitude, :swLatitude), POINT(:neLongitude, :neLatitude)), location)", nativeQuery = true)
+    List<Posts> postsContainedCurrentMap(@Param("swLatitude") String swLatitude, @Param("swLongitude")String swLongitude, @Param("neLatitude")String neLatitude, @Param("neLongitude")String neLongitude);
+
     List<Posts> findByUser_UserId(String userId);
 
     @Query("SELECT p.post FROM PostLikes p WHERE p.user.userId = :userId")
